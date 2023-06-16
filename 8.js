8d3e5fda13f7e6cb868118a2bf91c4ed704aeefe
d3st1H <133568473+d3st1H@users.noreply.github.com>
2023-06-16 09:15:45 +0300

    Delete 8.js

--- 8.js
+++
@@ -1,8 +0,0 @@
-let fruits = new Map;
-fruits.set("apple", "green")
-fruits.set("strawberry", "red")
-fruits.set("blueberry", "blue")
-
-fruits.forEach(function(value, key){
-    console.log(`Ключ - ${key}, Значение - ${value}`);
-})
